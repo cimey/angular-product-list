feb1cae1df5cf056c2ce4f42aa8aa41590dedec8
cimey <cumakilinc27@gmail.com>
2020-01-02 10:58:58 +0100

    3 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,22 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import {CartService} from "../services/cart.service";
+import { Component, OnInit } from "@angular/core";
+import { CartService } from "../services/cart.service";
+
+import { FormBuilder,FormGroup } from "@angular/forms";
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  selector: "app-cart",
+  templateUrl: "./cart.component.html",
+  styleUrls: ["./cart.component.css"]
 })
 export class CartComponent implements OnInit {
-
+  checkoutForm: FormGroup;
   products = [];
-  constructor(private cartService: CartService) { }
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.getAllItemsInCart();
+    this.checkoutForm = this.formBuilder.group({
+      name: 'ww',
+      id: 'w'
+    });
   }
 
-  getAllItemsInCart(){
-
+  getAllItemsInCart(): void {
     this.products = this.cartService.getItems();
   }
 
+  onSubmit(formValue){
+    window.alert( JSON.stringify (formValue));
+    this.cartService.clear();
+    this.checkoutForm.reset();
+  }
 }
